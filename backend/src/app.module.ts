668211679e8c85aeeddd48e383b215a74e9d850d
pedramasghari <54380437+pedramasghari@users.noreply.github.com>
2026-08-23 00:44:19 +0330

    feat: enable realtime notifications module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { WalletsModule } from './wallets/wallets.module';
 import { AuthModule } from './auth/auth.module';
 import { CommerceModule } from './commerce/commerce.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
-    UsersModule,
-    WalletsModule,
-    AuthModule,
-    CommerceModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, WalletsModule, AuthModule, CommerceModule, NotificationsModule],
 })
 export class AppModule {}
