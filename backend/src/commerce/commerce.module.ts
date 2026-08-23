fb203e005db2882806fa642315978bbd541123c8
pedramasghari <54380437+pedramasghari@users.noreply.github.com>
2026-08-23 19:39:21 +0330

    refactor(commerce): remove legacy admin bot dependency

--- backend/src/commerce/commerce.module.ts
+++ backend/src/commerce/commerce.module.ts
@@ -1,16 +1,32 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { WalletsModule } from '../wallets/wallets.module';
 import { NotificationsModule } from '../notifications/notifications.module';
-import { AdminBotModule } from '../admin-bot/admin-bot.module';
 import { User } from '../users/entities/user.entity';
 import { ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, Product, Service, WalletTransaction } from './entities/commerce.entity';
 import { CommerceService } from './commerce.service';
 import { CommerceController } from './commerce.controller';
 
 @Module({
-  imports: [AuthModule, WalletsModule, NotificationsModule, forwardRef(() => AdminBotModule), TypeOrmModule.forFeature([User, Service, Product, ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, WalletTransaction])],
+  imports: [
+    AuthModule,
+    WalletsModule,
+    NotificationsModule,
+    TypeOrmModule.forFeature([
+      User,
+      Service,
+      Product,
+      ActivationGuide,
+      ActivationProgress,
+      ActivationStep,
+      Order,
+      OrderInput,
+      PaymentMethod,
+      PaymentRequest,
+      WalletTransaction,
+    ]),
+  ],
   controllers: [CommerceController],
   providers: [CommerceService],
   exports: [CommerceService],
