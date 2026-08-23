78833b949e8e18a83ddd61b5dd605797bf2ae796
pedramasghari <54380437+pedramasghari@users.noreply.github.com>
2026-08-23 18:07:07 +0330

    refactor(commerce): move admin bot ownership to AdminBotModule

--- backend/src/commerce/commerce.module.ts
+++ backend/src/commerce/commerce.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { WalletsModule } from '../wallets/wallets.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { AdminBotModule } from '../admin-bot/admin-bot.module';
 import { User } from '../users/entities/user.entity';
 import { ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, Product, Service, WalletTransaction } from './entities/commerce.entity';
 import { CommerceService } from './commerce.service';
 import { CommerceController } from './commerce.controller';
-import { AdminBotService } from './admin-bot.service';
 
 @Module({
-  imports: [AuthModule, WalletsModule, NotificationsModule, TypeOrmModule.forFeature([User, Service, Product, ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, WalletTransaction])],
+  imports: [AuthModule, WalletsModule, NotificationsModule, forwardRef(() => AdminBotModule), TypeOrmModule.forFeature([User, Service, Product, ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, WalletTransaction])],
   controllers: [CommerceController],
-  providers: [CommerceService, AdminBotService],
+  providers: [CommerceService],
   exports: [CommerceService],
 })
 export class CommerceModule {}
