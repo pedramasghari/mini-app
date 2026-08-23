f4f4a52f30954a8ab701c87770babccc327d1beb
pedramasghari <54380437+pedramasghari@users.noreply.github.com>
2026-08-24 00:46:42 +0330

    fix(commerce): remove duplicate service entity import

--- backend/src/commerce/commerce.module.ts
+++ backend/src/commerce/commerce.module.ts
@@ -4,7 +4,7 @@ import { AuthModule } from '../auth/auth.module';
 import { WalletsModule } from '../wallets/wallets.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { User } from '../users/entities/user.entity';
-import { ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, Product, Service, ServiceSmsConfig, Service, SmsCodeOrder, SmsCodeWebhookEvent, WalletTransaction } from './entities/commerce.entity';
+import { ActivationGuide, ActivationProgress, ActivationStep, Order, OrderInput, PaymentMethod, PaymentRequest, Product, Service, ServiceSmsConfig, SmsCodeOrder, SmsCodeWebhookEvent, WalletTransaction } from './entities/commerce.entity';
 import { CommerceService } from './commerce.service';
 import { CommerceController } from './commerce.controller';
 import { SmsCodeService } from './smscode.service';
