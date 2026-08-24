5790b0bc0f8678bed0ef6b5547d843c4e1eb64de
pedramasghari <54380437+pedramasghari@users.noreply.github.com>
2026-08-24 23:16:55 +0330

    feat(admin): register withdrawal management controller

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -2,22 +2,26 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { CommerceModule } from '../commerce/commerce.module';
+import { WithdrawalsModule } from '../withdrawals/withdrawals.module';
 import { User } from '../users/entities/user.entity';
 import { Wallet } from '../wallets/entities/wallet.entity';
 import { ActivationProgress, Order, OrderInput, PaymentRequest, Product, Service, SmsCodeOrder, WalletTransaction } from '../commerce/entities/commerce.entity';
+import { WithdrawalRequest } from '../withdrawals/entities/withdrawal-request.entity';
 import { AdminController } from './admin.controller';
 import { AdminFinanceController } from './admin-finance.controller';
 import { AdminUsersController } from './admin-users.controller';
 import { AdminOrdersController } from './admin-orders.controller';
+import { AdminWithdrawalsController } from './admin-withdrawals.controller';
 import { AdminGuard } from './admin.guard';
 
 @Module({
   imports: [
     AuthModule,
     CommerceModule,
-    TypeOrmModule.forFeature([User, Wallet, WalletTransaction, Order, OrderInput, Product, Service, SmsCodeOrder, PaymentRequest, ActivationProgress]),
+    WithdrawalsModule,
+    TypeOrmModule.forFeature([User, Wallet, WalletTransaction, Order, OrderInput, Product, Service, SmsCodeOrder, PaymentRequest, ActivationProgress, WithdrawalRequest]),
   ],
-  controllers: [AdminController, AdminFinanceController, AdminUsersController, AdminOrdersController],
+  controllers: [AdminController, AdminFinanceController, AdminUsersController, AdminOrdersController, AdminWithdrawalsController],
   providers: [AdminGuard],
   exports: [AdminGuard],
 })
