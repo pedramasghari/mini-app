fcbe6901b107e36090b0f7f1d236a7389e68b04e
pedramasghari <54380437+pedramasghari@users.noreply.github.com>
2026-08-29 14:15:04 +0330

    1

---
+++ frontend/app/zibal/callback/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ZibalCallback() {
+  useEffect(() => {
+    window.location.replace("https://t.me/AkoID_bot/app");
+  }, []);
+
+  return null;
+}
